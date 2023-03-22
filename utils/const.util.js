6146e0eb1d9e4c1055f343de475aa908489208ac
Titou <nestor.2005@gmail.com>
2023-03-22 13:00:23 -0300

    update meli uris

--- utils/const.util.js
+++ utils/const.util.js
@@ -1,4 +1,5 @@
 exports.URI = {
   MELI_SEARCH: 'https://api.mercadolibre.com/sites/MLA/search', // ?q=
-  MELI_ITEMS: 'https://api.mercadolibre.com/items' ​// /:id - /:id​/description
+  MELI_ITEM: 'https://api.mercadolibre.com/items', // /:id - /:id​/description
+  MELI_CURRENCIES: 'https://api.mercadolibre.com/currencies'
 };
