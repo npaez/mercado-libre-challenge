c3efbb6cc0f6db207ef8661b10628a8c4f51abf1
Titou <nestor.2005@gmail.com>
2023-03-22 13:01:12 -0300

    update meli ctrl

---
+++ controllers/mercadolibre.controller.js
@@ -0,0 +1,87 @@
+// meli services
+const mlServices = require('../services/mercadolibre.service');
+
+// util methods
+const mlUtils = require('../utils/mercadolibre.utils');
+
+// controllers definition
+exports.search = async (req, res) => {
+  // variables
+  let rawRecordset;
+  let currencies;
+
+  // fetch recordsets
+  try {
+    rawRecordset = await mlServices.fetchSearch(
+      req.query.q || ''
+    );
+  } catch (ex) {
+    return res.failure(-1, ex.messsage, 500);
+  };
+
+  // fetch currencies
+  try {
+    currencies = mlUtils.processCurrencies(
+      await mlServices.fetchCurrencies()
+    );
+  } catch(ex) {
+    return res.failure(-1, ex.message, 500);
+  };
+
+  // process recordset
+  const items = mlUtils.processItems(
+    rawRecordset,
+    currencies
+  );
+
+  const categories = mlUtils.processCategories(
+    rawRecordset
+  );
+
+  return res.success({
+    items,
+    categories
+  }, 200);
+};
+
+exports.item = async (req, res) => {
+  let rawItem;
+  let rawDescription;
+  let rawCurrency;
+
+  // query item detail
+  try {
+    rawItem = await mlServices.fetchItem(
+      req.params.id
+    );
+  } catch(ex) {
+    return res.failure(-1, ex.message, 500);
+  };
+
+  // query item description
+  try {
+    rawDescription = await mlServices.fetchItemDescription(
+      req.params.id
+    );
+  } catch(ex) {
+    return res.failure(-1, ex.message, 500);
+  };
+
+  // query item currency
+  try {
+    rawCurrency = await mlServices.fetchCurrencies(
+      rawItem.currency_id
+    );
+  } catch(ex) {
+    return res.failure(-1, ex.message, 500);
+  };
+
+  // process detail
+  const item = mlUtils.processSingleItem(
+    rawItem,
+    rawDescription,
+    rawCurrency
+  );
+
+  return res.success(item, 200);
+};
