f7cccaf2e31056512538a71590fb268c746b3f49
Titou <nestor.2005@gmail.com>
2023-03-23 15:52:56 -0300

    add meli categori api uri

--- utils/const.util.js
+++ utils/const.util.js
@@ -1,5 +1,6 @@
 exports.URI = {
   MELI_SEARCH: 'https://api.mercadolibre.com/sites/MLA/search', // ?q=
   MELI_ITEM: 'https://api.mercadolibre.com/items', // /:id - /:id​/description
-  MELI_CURRENCIES: 'https://api.mercadolibre.com/currencies'
+  MELI_CURRENCIES: 'https://api.mercadolibre.com/currencies',
+  MELI_CATEGORY: 'https://api.mercadolibre.com/categories' // /:id
 };
