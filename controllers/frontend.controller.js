007e4ea0bd5ae0c51db75a1c8b1123e1c9484472
Titou <nestor.2005@gmail.com>
2023-03-24 10:36:23 -0300

    fix generate frontend path

--- controllers/frontend.controller.js
+++ controllers/frontend.controller.js
@@ -3,7 +3,7 @@ const path = require('path');
 
 // frontend client ctrl
 exports.client = (req, res) => {
-  const frontend = path.resolve(__dirname, '../../client/build', 'index.html');
+  const frontend = path.join(__dirname, '../client/build/index.html');
 
   return res.sendFile(frontend);
 };
